2e64331ba31a5cc03d0ba40796eafb5ef72fa6a6
presmelito <prvillanuevajr@gmail.com>
2025-02-19 20:55:28 +0800

    Main frame class cleanup

---
+++ src/main/java/com/esspi/hcbptool/ui/MainMenuBarController.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.esspi.hcbptool.ui;
+
+import com.esspi.hcbptool.MainFrame;
+import com.esspi.hcbptool.config.ToolConfig;
+import com.esspi.hcbptool.constants.Constants;
+import java.util.Objects;
+import javax.swing.JFileChooser;
+
+/**
+ *
+ * @author presmelito
+ */
+public class MainMenuBarController {
+
+    private static MainMenuBarController instance;
+
+    public static MainMenuBarController getInstance() {
+        if (Objects.nonNull(instance)) {
+            return instance;
+        }
+        return instance = new MainMenuBarController();
+    }
+
+    public MainMenuBarController() {
+        addComponentListeners();
+    }
+
+    public void init() {
+    }
+
+    public void changeRepoPathListener() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int option = fileChooser.showDialog(MainFrame.getInstance(), "Select");
+        if (JFileChooser.APPROVE_OPTION == option) {
+            ToolConfig.getInstance().setRepoPath(fileChooser.getSelectedFile().toPath());
+            MainFrame.getInstance().getRepoPathTf().setText(Constants.REPOPATH_INITIAL_TEXT + ToolConfig.getInstance().getRepoPath().toString());
+        }
+    }
+    
+    public void changeWorkspacePathListener(){
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int option = fileChooser.showDialog(MainFrame.getInstance(), "Select");
+        if (JFileChooser.APPROVE_OPTION == option) {
+            ToolConfig.getInstance().setWorkspacePath(fileChooser.getSelectedFile().toPath());
+            MainFrame.getInstance().getWorkspacePathTf().setText(Constants.WORKSPACEPATH_INITIAL_TEXT + ToolConfig.getInstance().getWorkspacePath().toString());
+        }
+    }
+
+    private void addComponentListeners() {
+        MainFrame.getInstance().getExitMenuItemBtn().setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_E, java.awt.event.InputEvent.CTRL_DOWN_MASK));
+        MainFrame.getInstance().getSaveConfigMenuItem().setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.CTRL_DOWN_MASK));
+        MainFrame.getInstance().getChangeWorkspacePathMenuItem().addActionListener(evt -> changeWorkspacePathListener());
+        MainFrame.getInstance().getChangeRepoPathMenuItem().addActionListener(evt -> changeRepoPathListener());
+    }
+}
---
+++ src/main/java/com/esspi/hcbptool/ui/SetDBPanelController.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.esspi.hcbptool.ui;
+
+import com.esspi.hcbptool.MainFrame;
+import com.esspi.hcbptool.config.DBConfig;
+import com.esspi.hcbptool.config.ToolConfig;
+import com.esspi.hcbptool.task.SetDbConfigTask;
+import com.esspi.hcbptool.task.TaskNotifier;
+import java.util.Objects;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ *
+ * @author presmelito
+ */
+public class SetDBPanelController {
+
+    private static SetDBPanelController instance;
+
+    public static SetDBPanelController getInstance() {
+        if (Objects.nonNull(instance)) {
+            return instance;
+        }
+        return  instance = new SetDBPanelController();
+    }
+
+    public SetDBPanelController() {
+        MainFrame.getInstance().getDbConfigTable().setModel((DefaultTableModel) MainFrame.getInstance().getDbConfigTable().getModel());
+        MainFrame.getInstance().getSetBtnSDB().addActionListener(e -> setBtnSDBActionPerformed(e));
+        MainFrame.getInstance().getRemoveBtnSDB().addActionListener(e -> this.removeBtnSDBActionPerformed(e));
+    }
+    
+    
+
+    public void buildSetDbPanel() {
+        String[] dbs = new String[ToolConfig.getInstance().getDbConfigs().size()];
+        DefaultTableModel model = (DefaultTableModel) MainFrame.getInstance().getDbConfigTable().getModel();
+        ((DefaultTableModel) model).setRowCount(0);
+        for (int i = 0; i < ToolConfig.getInstance().getDbConfigs().size(); i++) {
+            dbs[i] = ToolConfig.getInstance().getDbConfigs().get(i).toString();
+            model.addRow(new String[]{
+                ToolConfig.getInstance().getDbConfigs().get(i).getName(),
+                ToolConfig.getInstance().getDbConfigs().get(i).getHost(),
+                ToolConfig.getInstance().getDbConfigs().get(i).getPort(),
+                ToolConfig.getInstance().getDbConfigs().get(i).getDbName(),});
+        }
+    }
+
+    public void setDb(DBConfig dbConfig) {
+        if (JOptionPane.YES_OPTION == JOptionPane.showConfirmDialog(MainFrame.getInstance().getSetDbPanel(), "Apply " + dbConfig.getName(), "Set DB Config", JOptionPane.YES_NO_OPTION)) {
+            JDialog dialogLoader = new JOptionPane("Applying " + dbConfig.getName(), JOptionPane.INFORMATION_MESSAGE, JOptionPane.PLAIN_MESSAGE, null, new Object[]{}).createDialog(MainFrame.getInstance().getSetDbPanel(), "Applying DB Config...");
+            dialogLoader.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+            SetDbConfigTask setDbConfigTask = new SetDbConfigTask(dbConfig);
+            new TaskNotifier().setFutures(setDbConfigTask.run()).setDoAfter(() -> {
+                dialogLoader.dispose();
+                JOptionPane.showMessageDialog(MainFrame.getInstance().getSetDbPanel(), dbConfig.getName() + " DB Applied!");
+            }).listen();
+            dialogLoader.setVisible(Boolean.TRUE);
+        }
+    }
+
+    public void setBtnSDBActionPerformed(java.awt.event.ActionEvent evt) {
+        if (MainFrame.getInstance().getDbConfigTable().getSelectedRow() == -1) {
+            JOptionPane.showMessageDialog(MainFrame.getInstance().getSetDbPanel(), "Please select a config");
+            return;
+        }
+        DBConfig dbConfig = ToolConfig.getInstance().getDbConfigs().get(MainFrame.getInstance().getDbConfigTable().getSelectedRow());
+        SetDBPanelController.getInstance().setDb(dbConfig);
+    }
+
+    private void removeBtnSDBActionPerformed(java.awt.event.ActionEvent evt) {
+        if (MainFrame.getInstance().getDbConfigTable().getSelectedRow() != -1) {
+            int result = JOptionPane.showConfirmDialog(MainFrame.getInstance().getSetDbPanel(),
+                    "Remove this Config?", "Remove Set DB Config", JOptionPane.YES_NO_OPTION);
+            if (result == JOptionPane.YES_OPTION) {
+                DBConfig dbconfig = ToolConfig.getInstance().getDbConfigs().remove(MainFrame.getInstance().getDbConfigTable().getSelectedRow());
+                DefaultTableModel model = (DefaultTableModel) MainFrame.getInstance().getDbConfigTable().getModel();
+                model.removeRow(MainFrame.getInstance().getDbConfigTable().getSelectedRow());
+                SystemTrayController.getInstance().removeDBConfigFromSetDBTrayMenu(dbconfig);
+            }
+        } else {
+            JOptionPane.showMessageDialog(MainFrame.getInstance().getSetDbPanel(), "Please select a config");
+        }
+    }
+
+    public boolean addDbHasMissingField() {
+        boolean hasMissingField = false;
+        if (StringUtils.isEmpty(MainFrame.getInstance().getNameFieldADB().getText())
+                || StringUtils.isEmpty(MainFrame.getInstance().getDbNameFieldADB().getText())
+                || StringUtils.isEmpty(MainFrame.getInstance().getAdminIdFieldADB().getText())
+                || StringUtils.isEmpty(MainFrame.getInstance().getAdminPassFieldADB().getText())
+                || StringUtils.isEmpty(MainFrame.getInstance().getUserIdFieldADB().getText())
+                || StringUtils.isEmpty(MainFrame.getInstance().getUserPassFieldADB().getText())
+                || StringUtils.isEmpty(MainFrame.getInstance().getPortFieldADB().getText())
+                || StringUtils.isEmpty(MainFrame.getInstance().getHostFieldADB().getText())) {
+            JOptionPane.showMessageDialog(MainFrame.getInstance().getRootPane(), "All Fields Are Required");
+            hasMissingField = true;
+        }
+        return hasMissingField;
+    }
+}
