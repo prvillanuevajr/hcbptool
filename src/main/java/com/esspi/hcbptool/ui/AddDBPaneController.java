2fc58f23f6498b5354adb2cd5c3a09559b601718
presmelito <prvillanuevajr@gmail.com>
2025-03-24 01:11:43 +0800

    fix validation

--- src/main/java/com/esspi/hcbptool/ui/AddDBPaneController.java
+++ src/main/java/com/esspi/hcbptool/ui/AddDBPaneController.java
@@ -15,6 +15,7 @@ import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.function.Consumer;
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
@@ -46,18 +47,15 @@ public class AddDBPaneController {
         dialogLoader.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
         JOptionPane jop = (JOptionPane) dialogLoader.getContentPane().getComponent(0);
         Task validateDBConfigTask = new ValidateDBConfigTask(dBConfig);
-        validateDBConfigTask.setOnError(message -> {
+        Consumer<String> doAfter = message -> {
             jop.setMessage(message);
             jop.setOptions(new Object[]{"OK"});
+            jop.setIcon(null);
             dialogLoader.pack();
             dialogLoader.setLocationRelativeTo(MainFrame.getInstance());
-        });
-        validateDBConfigTask.setOnSuccess(message -> {
-            jop.setMessage(message);
-            jop.setOptions(new Object[]{"OK"});
-            dialogLoader.pack();
-            dialogLoader.setLocationRelativeTo(MainFrame.getInstance());
-        });
+        };
+        validateDBConfigTask.setOnError(doAfter);
+        validateDBConfigTask.setOnSuccess(doAfter);
         TheExecutor.getInstance().getExecutorService().submit(validateDBConfigTask);
         dialogLoader.setVisible(true);
     }
@@ -76,6 +74,7 @@ public class AddDBPaneController {
         JOptionPane jOptionPane = (JOptionPane) dialogLoader.getContentPane().getComponent(0);
         dialogLoader.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
         validateTask.setOnSuccess(message -> {
+            dialogLoader.dispose();
             ToolConfig.getInstance().getDbConfigs().add(dBConfig);
             Arrays.stream(MainFrame.getInstance().getAddDbPanel().getComponents()).forEach(component -> {
                 if (component instanceof JTextField) {
